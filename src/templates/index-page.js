170fcb9396e2ef4eb4dccc6fe4a7a16d1f1b047f
Jared <jared@digitalnutt.com>
2020-01-03 14:49:22 -0800

    registering the project page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,47 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql, StaticQuery } from 'gatsby';
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from '../components/Layout';
+// import Features from '../components/Features';
+// import BlogRoll from '../components/BlogRoll';
+
+const ProjectCard = ({ project }) => (
+  <article>
+    <Link to={project.fields.slug}>
+      <h1>{project.frontmatter.title}</h1>
+    </Link>
+  </article>
+);
+
+const ProjectRoll = () => (
+  <StaticQuery
+    query={graphql`
+      query ProjectRollQuery {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: { frontmatter: { templateKey: { eq: "project" } } }
+        ) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={(data, count) => {
+      const { edges: projects } = data.allMarkdownRemark;
+      return <ProjectCard project={projects[0].node} />;
+    }}
+  />
+);
 
 export const IndexPageTemplate = ({
   image,
@@ -15,104 +52,132 @@ export const IndexPageTemplate = ({
   description,
   intro,
 }) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
+  <>
+    {/* <div */}
+    {/*   className="full-width-image margin-top-0" */}
+    {/*   style={{ */}
+    {/*     backgroundImage: `url(${ */}
+    {/*       !!image.childImageSharp ? image.childImageSharp.fluid.src : image */}
+    {/*     })`, */}
+    {/*     backgroundPosition: `top left`, */}
+    {/*     backgroundAttachment: `fixed`, */}
+    {/*   }} */}
+    {/* > */}
+    {/*   <div */}
+    {/*     style={{ */}
+    {/*       display: 'flex', */}
+    {/*       height: '150px', */}
+    {/*       lineHeight: '1', */}
+    {/*       justifyContent: 'space-around', */}
+    {/*       alignItems: 'left', */}
+    {/*       flexDirection: 'column', */}
+    {/*     }} */}
+    {/*   > */}
+    {/*     <h1 */}
+    {/*       className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen" */}
+    {/*       style={{ */}
+    {/*         boxShadow: */}
+    {/*           'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px', */}
+    {/*         backgroundColor: 'rgb(255, 68, 0)', */}
+    {/*         color: 'white', */}
+    {/*         lineHeight: '1', */}
+    {/*         padding: '0.25em', */}
+    {/*       }} */}
+    {/*     > */}
+    {/*       {title} */}
+    {/*     </h1> */}
+    {/*     <h3 */}
+    {/*       className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen" */}
+    {/*       style={{ */}
+    {/*         boxShadow: */}
+    {/*           'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px', */}
+    {/*         backgroundColor: 'rgb(255, 68, 0)', */}
+    {/*         color: 'white', */}
+    {/*         lineHeight: '1', */}
+    {/*         padding: '0.25em', */}
+    {/*       }} */}
+    {/*     > */}
+    {/*       {subheading} */}
+    {/*     </h3> */}
+    {/*   </div> */}
+    {/* </div> */}
+    <section className="section">
       <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <h1 className="title">Latest Projects</h1>
+        <ProjectRoll />
       </div>
     </section>
-  </div>
-)
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Skills</h1>
+        <p>Skills</p>
+        <h2>Bottom Line</h2>
+        <p>
+          I am a web developer who mainly works in Javascript. If you like
+          lists, keep reading!
+        </p>
+        <h2>Development</h2>
+        <h3>Everyday Languages</h3>
+        <p>Javascript, HTML, CSS</p>
+        <h3>Familiar Languages</h3>
+        <p>Swift, Kotlin, Python</p>
+      </div>
+    </section>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">About</h1>
+        <p>About</p>
+      </div>
+    </section>
+    {/* <section className="section section--gradient"> */}
+    {/*   <div className="container"> */}
+    {/*     <div className="section"> */}
+    {/*       <div className="columns"> */}
+    {/*         <div className="column is-10 is-offset-1"> */}
+    {/*           <div className="content"> */}
+    {/*             <div className="content"> */}
+    {/*               <div className="tile"> */}
+    {/*                 <h1 className="title">{mainpitch.title}</h1> */}
+    {/*               </div> */}
+    {/*               <div className="tile"> */}
+    {/*                 <h3 className="subtitle">{mainpitch.description}</h3> */}
+    {/*               </div> */}
+    {/*             </div> */}
+    {/*             <div className="columns"> */}
+    {/*               <div className="column is-12"> */}
+    {/*                 <h3 className="has-text-weight-semibold is-size-2"> */}
+    {/*                   {heading} */}
+    {/*                 </h3> */}
+    {/*                 <p>{description}</p> */}
+    {/*               </div> */}
+    {/*             </div> */}
+    {/*             <Features gridItems={intro.blurbs} /> */}
+    {/*             <div className="columns"> */}
+    {/*               <div className="column is-12 has-text-centered"> */}
+    {/*                 <Link className="btn" to="/products"> */}
+    {/*                   See all products */}
+    {/*                 </Link> */}
+    {/*               </div> */}
+    {/*             </div> */}
+    {/*             <div className="column is-12"> */}
+    {/*               <h3 className="has-text-weight-semibold is-size-2"> */}
+    {/*                 Latest stories */}
+    {/*               </h3> */}
+    {/*               <BlogRoll /> */}
+    {/*               <div className="column is-12 has-text-centered"> */}
+    {/*                 <Link className="btn" to="/blog"> */}
+    {/*                   Read more */}
+    {/*                 </Link> */}
+    {/*               </div> */}
+    {/*             </div> */}
+    {/*           </div> */}
+    {/*         </div> */}
+    {/*       </div> */}
+    {/*     </div> */}
+    {/*   </div> */}
+    {/* </section> */}
+  </>
+);
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -124,25 +189,25 @@ IndexPageTemplate.propTypes = {
   intro: PropTypes.shape({
     blurbs: PropTypes.array,
   }),
-}
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  // const { frontmatter } = data.markdownRemark;
 
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+      // image={frontmatter.image}
+      // title={frontmatter.title}
+      // heading={frontmatter.heading}
+      // subheading={frontmatter.subheading}
+      // mainpitch={frontmatter.mainpitch}
+      // description={frontmatter.description}
+      // intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -150,44 +215,44 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const pageQuery = graphql`
-  query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-      frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
-          description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query IndexPageTemplate {
+//     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+//       frontmatter {
+//         title
+//         image {
+//           childImageSharp {
+//             fluid(maxWidth: 2048, quality: 100) {
+//               ...GatsbyImageSharpFluid
+//             }
+//           }
+//         }
+//         heading
+//         subheading
+//         mainpitch {
+//           title
+//           description
+//         }
+//         description
+//         intro {
+//           blurbs {
+//             image {
+//               childImageSharp {
+//                 fluid(maxWidth: 240, quality: 64) {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//             text
+//           }
+//           heading
+//           description
+//         }
+//       }
+//     }
+//   }
+// `;
