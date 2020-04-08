5ae7b813ca53f642e4b8cb73dde586388b878486
Jared <jared@digitalnutt.com>
2020-04-08 13:54:26 -0700

    adding music

--- src/pages/art/index.js
+++ src/pages/art/index.js
@@ -12,14 +12,25 @@ export default class BlogIndexPage extends React.Component {
               <h1 className="title is-1 has-text-centered">Music</h1>
               <h2>Modulo</h2>
               <p>
-                I write music using <a href="https://sonic-pi.net/">Sonic Pi</a>. It is available on the following platforms:
+                I write music using <a href="https://sonic-pi.net/">Sonic Pi</a>. It is available on
+                <a href="https://open.spotify.com/artist/1o6CGTMPjk1C0IdK9jV2H1"> Spotify</a> and <a href="https://music.apple.com/us/artist/modulo/1499420471">Apple Music</a>
               </p>
               <h2>Playlists</h2>
-              <p>I curate playlists on Spotify to serve my musical needs.</p>
-              <h3>Instrumental Coding Sessions</h3>
-              <iframe src="https://open.spotify.com/embed/playlist/4hwEGdFGfxBiwUbVmjs9RG" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-              <h3>Heavy as a Really Heavy Thing</h3>
-              <iframe src="https://open.spotify.com/embed/playlist/6OuqEi3fzY8IqA6yBDe6kE" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+              <p>
+                I curate playlists on Spotify to serve my musical needs. Also happy to take reccomendations. Shoot them to me on <a href="https://twitter.com/code_nutt">twitter</a>.
+              </p>
+              <div className="columns">
+                <div className="column">
+                  <h3>Instrumental Coding Sessions</h3>
+                  <p>Curated collection of classical, score, electronic, video game and EPIC music. Best served on Random. Now with extra Jazz. Increases productivity by 1000% guaranteed!* *Unverifiable but possible</p>
+                  <iframe src="https://open.spotify.com/embed/playlist/4hwEGdFGfxBiwUbVmjs9RG" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                </div>
+                <div className="column">
+                  <h3>Heavy as a Really Heavy Thing</h3>
+                  <p>The heaviest/grooviest metal songs around. </p>
+                  <iframe src="https://open.spotify.com/embed/playlist/6OuqEi3fzY8IqA6yBDe6kE" width="100%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                </div>
+              </div>
             </div>
           </div>
         </section>
