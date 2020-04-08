d684fe9d8ca556a051c183d18f04d1e982cc4456
Jared <jared@digitalnutt.com>
2020-04-08 12:38:58 -0700

    added medium posts to teaching page

--- src/pages/teaching/index.js
+++ src/pages/teaching/index.js
@@ -1,6 +1,42 @@
 import React from "react";
-
+import { Link, graphql, StaticQuery } from "gatsby";
 import Layout from "../../components/Layout";
+
+const RecentPostsRoll = () => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query allMediumPostsQuery {
+          allMediumPost {
+            edges {
+              node {
+                id
+                title
+                virtuals {
+                  subtitle
+                  previewImage {
+                    imageId
+                  }
+                }
+                slug
+                medium_id
+              }
+            }
+          }
+        }
+      `}
+      render={(data, count) => {
+        return data.allMediumPost.edges.map(({ node }) => {
+          return (
+            <p>
+              <a href={`https://medium.com/@jarednutt27/${node.slug}-${node.medium_id}`}>{node.title}</a>
+            </p>
+          );
+        });
+      }}
+    />
+  );
+};
 
 export default class TeachingPage extends React.Component {
   render() {
@@ -15,7 +51,7 @@ export default class TeachingPage extends React.Component {
                 I write articles I wish I had when learning. I write on <a href="https://medium.com/@jarednutt27">Medium</a>, <a href="https://dev.to/codenutt">Practical Dev</a> and sometimes on <a href="https://www.freecodecamp.org/news/author/jared/">FreecodeCamp News</a>
               </p>
               <h3>Recent Posts</h3>
-              <p>Recent Posts</p>
+              <RecentPostsRoll />
               <h2>Video</h2>
               <h3>YouTube</h3>
               <p>I have a youtube channel dedicated to web development.</p>
